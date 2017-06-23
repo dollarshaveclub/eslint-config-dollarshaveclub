143e91911d2da018a5a3369bfda0b73c92628797
Brian Gonzalez <me@briangonzalez.org>
2017-06-22 17:39:22 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# eslint-config-dollarshaveclub
+
+> ESLint [shareable config](http://eslint.org/docs/developer-guide/shareable-configs.html) for Dollar Shave Club.
+
+
+## Installation
+
+```
+$ npm install --save-dev eslint eslint-config-dollarshaveclub
+```
+
+
+## Usage
+
+Once the `eslint-config-dollarshaveclub` package is installed, you can use it by specifying `dollarshaveclub` in the [`extends`](http://eslint.org/docs/user-guide/configuring#extending-configuration-files) section of your [ESLint configuration](http://eslint.org/docs/user-guide/configuring).
+
+```js
+{
+  "extends": "dollarshaveclub",
+  "rules": {
+    // Additional, per-project rules...
+  }
+}
+```
+
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+module.exports = {
+  extends: [
+    'standard',
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'no-plusplus': 0,
+    'no-restricted-syntax': 0,
+    'comma-dangle': [2, 'always-multiline'],
+  },
+  configs: {
+    parserOptions: {
+      ecmaVersion: 7,
+      sourceType: 'module',
+      ecmaFeatures: {
+        experimentalObjectRestSpread: true,
+        modules: true,
+      },
+    },
+    ember: {
+      rules: {
+        'dollarshaveclub/spread-mixins': 2,
+        'no-underscore-dangle': [2, { allow: ['_super'] }],
+        'consistent-return': 0,
+      },
+    },
+  },
+}
