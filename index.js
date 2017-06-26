35663e7ee720273259863047173f393950909257
Brian Gonzalez <me@briangonzalez.org>
2017-06-26 16:14:49 -0700

    bring in more custom configs

--- index.js
+++ index.js
@@ -11,9 +11,16 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     'import/no-unresolved': 0,
     'import/extensions': 0,
+    'import/prefer-default-export': 0,
     'no-plusplus': 0,
     'no-restricted-syntax': 0,
     'comma-dangle': [0, 'always-multiline'],
+    'camelcase': 0,
+    'func-names': 1,
+    'no-await-in-loop': 0,
+    'no-multi-assign': 0,
+    'no-underscore-dangle': 0,
+    'quotes': 0,
   },
   configs: {
     parserOptions: {
