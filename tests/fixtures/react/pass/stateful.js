4d8565563ebc7bca8792d45125e6fa5444c6eb0d
jongleberry <me@jongleberry.com>
2017-08-14 13:15:29 -0700

    add some tests

---
+++ tests/fixtures/react/pass/stateful.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react'
+
+export default class Test extends Component {
+  state = {
+    rendered: false
+  }
+
+  onClick = (e) => {
+    e.preventDefault()
+  }
+
+  render () {
+    return (
+      <a className='hi' href='#' onClick={this.onClick}>CLICK ME</a>
+    )
+  }
+}
