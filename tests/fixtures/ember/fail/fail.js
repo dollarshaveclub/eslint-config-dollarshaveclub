5eae8d688491791094793022c7cdd733e00eab7f
Arjan Singh <arjan.singh@dollarshaveclub.com>
2017-09-28 17:50:55 -0700

    More consistent Ember lint rules (#16)
    * eslint@^4.7.0
    
    * More consistent lint rules
    
    * 2.3.0

---
+++ tests/fixtures/ember/fail/fail.js
@@ -0,0 +1,19 @@
+const multiLineArrayLastBracket = [
+  1,
+  2,
+  3 ]
+
+const multiLineArrayFirstBracket = [
+  1, 2, 3 ]
+
+let x = 2
+
+if (x === 3) {
+  console.log('foo')
+} else if (multiLineArrayFirstBracket === multiLineArrayLastBracket) {
+  console.log('bar')
+} else {
+  if (x === 2) {
+    console.log('baz')
+  }
+}
