16a0d73fc812af2f8e835e0e2a7bb51393e695c2
Jeff Wainwright <yowainwright@gmail.com>
2018-01-10 14:39:42 -0800

    Quote patch (#27)
    * [update] override quotes
    
    * [version] version patch

--- tests/fixtures/base/fail/fail.js
+++ tests/fixtures/base/fail/fail.js
@@ -1,5 +1,4 @@
-
-const noSemiColons = 'please'
+const noSemiColons = 'please';
 
 const trailingCommasPlease = [
   1,
@@ -9,5 +8,8 @@ const trailingCommasPlease = [
 
 let shouldbeConst = 'shouldbeConst'
 
+const shouldbeSingleQuote = "shouldbeSingleQuote"
+
 console.log(trailingCommasPlease)
 console.log(shouldbeConst)
+console.log(shouldbeSingleQuote)
