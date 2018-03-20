d08bdc482071888d67fdcd4570607f3415eb9ede
Jeff Wainwright <yowainwright@gmail.com>
2018-03-19 20:19:53 -0700

    [init] adds initial `eslint-plugin-jsx-a11y` plugin

--- react.js
+++ react.js
@@ -5,4 +5,16 @@ module.exports = {
     require.resolve('eslint-config-standard-react'),
     require.resolve('./'),
   ],
+  plugins: [
+   'jsx-a11y'
+  ],
+  rules: {
+    'jsx-a11y/label-has-for': [ 2, {
+      'components': [ 'Label' ],
+      'required': {
+          'every': [ 'id' ]
+      },
+      'allowChildren': false,
+    }],
+  },
 }
