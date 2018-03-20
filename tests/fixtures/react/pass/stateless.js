53f41719de153d17581485ba9a637174a3caa50e
Jeff Wainwright <yowainwright@gmail.com>
2018-03-20 11:26:38 -0700

    [init] adds initial `eslint-plugin-jsx-a11y` plugin (#36)

--- tests/fixtures/react/pass/stateless.js
+++ tests/fixtures/react/pass/stateless.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const Thing = ({ prop1, prop2 }) => (
-  <div className='something'>{prop1}{prop2}</div>
+  <div className='something'><label htmlFor={prop1}>{prop1}</label>{prop2}</div>
 )
 
 Thing.propTypes = {
